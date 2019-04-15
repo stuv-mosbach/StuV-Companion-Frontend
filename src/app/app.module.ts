f4210e15b567e6186c74d2931588816fa2028f36
sIPeye <fobytobi@gmail.com>
2019-04-15 22:09:11 +0200

    added ionic selectable package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage';
+import { IonicSelectableModule } from 'ionic-selectable';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot(), IonicSelectableModule],
   providers: [
     StatusBar,
     SplashScreen,
