ed3b039fb9f237a3cb970f3d079830c52983c7b3
Supporterino <lars@roth-kl.de>
2019-04-23 20:13:28 +0200

    Working on new settings page

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { Router } from '@angular/router';
+import {ThemeService} from '../theming/theme.service';
+import {Theme} from '../theming/theme.model';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ToastController } from '@ionic/angular';
+import { SettingsService } from './settings.service';
 
 @Component({
   selector: 'app-settings',
@@ -8,23 +13,52 @@ import { Router } from '@angular/router';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
-  course: string;
+  courses: any[];
+  themes: Theme[];
 
-  constructor(private storage: Storage, private router: Router) { }
+  selectedCourse: string;
+  selectedTheme: Theme;
+
+  constructor(
+    private settingsService: SettingsService,
+    private toastController: ToastController,
+    private storage: Storage,
+    private router: Router,
+    private themeService: ThemeService,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
-        this.storage.get('course').then((data) => {
-            this.course = data;
-        });
+    // this.startForm = this.fb.group({
+    //     course: ['', [
+    //         Validators.required
+    //     ]],
+    //     theme: ['', [
+    //         Validators.required
+    //     ]]
+    // });
   }
 
-  reset() {
-    this.storage.set('active', 'no')
-    .then(() => {
-      this.storage.remove("course")
-      .then(() => {
-        this.router.navigate(['start']);
-      });
+  ionViewDidEnter() {
+    this.settingsService.getAllCourses().subscribe((list: string[]) => {
+      this.courses = list;
+      this.courses.sort();
     });
+    this.themes = this.themeService.getAllThemes();
+    this.themeService.getTheme().subscribe((data: Theme) => {
+      selectedTheme = data;
+    })
+    this.storage.get('course').then((data: string) => {
+      selectedCourse = data;
+    });
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      showCloseButton: true,
+      closeButtonText: 'Dismiss',
+      duration: 2000
+    });
+    toast.present();
   }
 }
